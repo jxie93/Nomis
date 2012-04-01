a9e6c8cb84383366f91c11aa7c82760efd959448
Jason Xie <zhihao420@hotmail.com>
2012-04-01 21:30:22 +0100

    record object

---
+++ src/Record.java
@@ -0,0 +1,30 @@
+
+public class Record {
+	private int rank;
+	private int marks;
+	private int levels;
+	
+	public Record(int rank,int marks,int levels)
+	{
+		this.rank = rank;
+		this.marks =marks;
+		this.levels =levels;
+	}
+	
+	public int getrank()
+	{
+		return rank;
+	}
+	
+	public int getmarks()
+	{
+		return marks;
+	}
+	
+	public int getlevels()
+	{
+		return levels;
+	}
+	
+	
+}
